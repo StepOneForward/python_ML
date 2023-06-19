56e95beec4f2753c44560eb2b1c60e7004b8e433
StepOneForward <onestepforwardgames@gmail.com>
2023-06-20 01:02:41 +0400

    recursion

--- lesson.py
+++ lesson.py
@@ -1,3 +1,10 @@
 
-var= "James Bond"
-print(var[2::-1])
+nu = 1234
+def funct (number: int):
+    next = number % 10
+    num = int(number / 10)
+    if num > 0:
+        result = (next * 10) + funct(num)
+    return result
+
+print(funct(nu))
